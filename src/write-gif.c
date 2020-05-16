9c19f77cb7e4e36b2e58589b2e8dd00d409953ee
Sebastien Tricaud <sebastien@honeynet.org>
2020-05-16 00:30:36 -0700

    Adding a few more things

--- src/write-gif.c
+++ src/write-gif.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <gd.h>
 #include <gdfontl.h>
 
@@ -8,30 +9,39 @@
 
 #define KEYFRAMES 10
 
-static void _allocate_colors(gdImagePtr im)
-{
-	t2g_white = gdImageColorAllocate(im, 255, 255, 255);
-	t2g_black = gdImageColorAllocate(im, 0, 0, 0);
-	t2g_blue = gdImageColorAllocate(im, 0, 98, 174);
-}
+#define BRECT_LOWER_LEFT_X 0
+#define BRECT_LOWER_LEFT_Y 1
+#define BRECT_LOWER_RIGHT_X 2
+#define BRECT_LOWER_RIGHT_Y 3
+#define BRECT_UPPER_RIGHT_X 4
+#define BRECT_UPPER_RIGHT_Y 5
+#define BRECT_UPPER_LEFT_X 6
+#define BRECT_UPPER_LEFT_Y 7
+
+
+//#define DEFAULT_FONT "/usr/share/fonts/gnu-free/FreeMono.ttf"
+#define DEFAULT_FONT "/usr/share/wine/fonts/times.ttf"
 
 static gdImagePtr _new_image_with_background(t2g_t *t2g, gdImagePtr im, char r, char g, char b)
 {
-	im = gdImageCreate(t2g->width, t2g->height);
+	im = gdImageCreateTrueColor(t2g->width, t2g->height);
 	if (!im) {
+		fprintf(stderr, "Error: Could not create TrueColor image\n");
 		return NULL;
 	}
-	gdImageColorAllocate(im, r, g, b);
+	gdImageAlphaBlending(im, gdEffectReplace);
+	gdImageFilledRectangle(im, 0, 0, t2g->width, t2g->height, gdTrueColorAlpha(255,255,255,0));
 	return im;
 }
 
 static gdImagePtr _write_in_every_frame(t2g_t *t2g, gdImagePtr im)
 {
-	int black;
 
 	_new_image_with_background(t2g, im, 255, 255, 255);
 	
-	gdImageLine(im, 0, 0+(t2g->height/2), t2g->width, 0+(t2g->height/2), t2g_black); // Timeline line
+	gdImageLine(im, 0, t2g->height/2, t2g->width, t2g->height/2, gdTrueColorAlpha(0,0,0,0)); // Timeline line
+	/* gdImageColorTransparent (im, gdTrueColorAlpha(0,0,0,0)); */
+	gdImageSetAntiAliased (im, gdTrueColorAlpha(0,0,0,0));
 	return im;
 }
 
@@ -42,11 +52,58 @@ static int _write_single_object(FILE *out, t2g_t *t2g, gdImagePtr im)
 	int delay = 6;
 	int ypos = 0;
 	int xpos = 0;
+	int brect[8];
+	int rect_p = 6; // Proportion of the rectangle/image
 	
 	char framepos;
 	for (framepos = 0; framepos < KEYFRAMES; framepos++) {
 		im = _write_in_every_frame(t2g, im);
 
+		switch(framepos) {
+		case 0:
+
+			gdImageSetThickness(im, 4);
+			gdImageFilledRectangle(im, t2g->width/2-t2g->width/rect_p, t2g->height/2-t2g->height/rect_p, t2g->width/2+t2g->width/rect_p, t2g->height/2+t2g->width/rect_p, gdTrueColorAlpha(255,255,255,0));
+			gdImageRectangle(im, t2g->width/2-t2g->width/rect_p, t2g->height/2-t2g->height/rect_p, t2g->width/2+t2g->width/rect_p, t2g->height/2+t2g->width/rect_p, gdTrueColorAlpha(0,0,0,0));
+			gdImageSetThickness(im, 1);
+			
+			gdImageStringFT(NULL, brect, 0,
+					DEFAULT_FONT, 16,
+					0,
+					0, 0,
+					t2g->label_text);
+			int text_width = brect[BRECT_LOWER_RIGHT_X] - brect[BRECT_LOWER_LEFT_X];
+			int text_height = brect[BRECT_LOWER_LEFT_Y] - brect[BRECT_UPPER_LEFT_Y];
+			int text_center_pos_x = text_width / 2;
+			int text_center_pos_y = text_height / 2;
+			gdImageStringFT(im, brect, gdTrueColorAlpha(0,0,0,0),
+					DEFAULT_FONT, 16,
+					0,
+					t2g->width/2-text_center_pos_x, t2g->height/2-text_center_pos_y,
+					t2g->label_text);
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			break;
+			
+		default:
+			fprintf(stderr, "Uh? Should nevery see this!\n");
+		}
 		
 		
 		gdImageGifAnimAdd(im, out, 1, 0, 0, delay, 0, NULL);
@@ -121,7 +178,6 @@ int write_gif(t2g_t *t2g, char *outgif)
 	} 	
 
 	im = _new_image_with_background(t2g, im, 255, 255, 255);
-	_allocate_colors(im);
 	
 	gdImageGifAnimBegin(im, out, 1, -1);
 	
